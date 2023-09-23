ac39278872e58fb71aba48b71bd1551b62afd43c
sa1mont <93553875+sa1mont@users.noreply.github.com>
2023-09-23 23:43:07 +0300

    Добавил счётчик
    Добавил счётчик;
    Дополнил приветственное сообщение;
    Добавил в конечное сообщение f-строку.

--- Guess the number.py
+++ Guess the number.py
@@ -1,6 +1,6 @@
 import random
 number = random.randint(1, 101)
-print("Добро пожаловать в числовую угадайку")
+print("Добро пожаловать в числовую угадайку. Мной было загадано число, попробуй ка отгадать.")
 
 def is_valid(num):
     if num.isdigit() and 1 <= int(num) <= 100:
@@ -9,19 +9,20 @@ def is_valid(num):
         return False
 
 flag_1 = False
-
+total = 0
 while flag_1 == False:
     n = input("Введите число от 1 до 100")
     if is_valid(n):
         n = int(n)
+        total += 1
         if n > number:
             print('Ваше число больше загаданного, попробуйте еще разок')
             n = input()
         elif n < number:
             print('Ваше число меньше загаданного, попробуйте еще разок')
-        else:
-            print('Верно, Вы угадали! Поздравляем!')
+        elif n == number:
+            print(f'Верно, Вы угадали с {total} попытки! Поздравляем!')
             flag_1 = True
-            break       
+            break
     else:
         print('Ввод некорректный. Введите число от 1 до 100')
